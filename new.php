680d9c19aa6eadeced8a820e57adb60c73ec6c20
Taghreed-Hassan <mohamadelsayed622@gmail.com>
2020-10-04 15:29:28 +0200

    first commit

---
+++ new.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+//<?php
+// PHP code goes here
+//?>*/
+//1. Write a PHP script to get the PHP version and configuration information
+
+
+echo 'helloww';
+
+phpinfo();
+
+
+
---
+++ syntax.php
@@ -0,0 +1,20 @@
+<?php  
+ECHO "Hello World!<br>";
+echo "Hello World!<br>";
+EcHo "Hello World!<br>";
+
+echo"<br>";
+$txt = "W3Schools.com";
+echo "I love $txt!";
+
+/*
+ $color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
+//----------------------------
+*/
+echo"<br>";
+echo "Tomorrow I \'ll learn PHP global variables."."\n"; 
+echo "This is a bad command *"."\n"; 
+
